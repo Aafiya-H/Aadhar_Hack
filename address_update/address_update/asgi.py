cd35e72b547d026dea9a9889c3f6be7e34ee078a
Aafiya <aafiya.hussain2@gmail.com>
2021-10-28 20:21:04 +0530

    webapp initialized

---
+++ address_update/address_update/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for address_update project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'address_update.settings')
+
+application = get_asgi_application()
