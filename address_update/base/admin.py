5a5eb2030ffcaa4646f1083ec73ab3e9c6c71aa4
Aafiya <aafiya.hussain2@gmail.com>
2021-10-29 12:30:09 +0530

    auth done

--- address_update/base/admin.py
+++ address_update/base/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.models import User
 
 admin.site.register(CustomUser)
--- address_update/base/views.py
+++ address_update/base/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.contrib.auth.models import User
 from .models import *
 from .serializers import *
 from .decorators import *
@@ -21,7 +20,7 @@ def login(request):
 		email_id = json_data['email_id']
 		password = json_data['password']
 		# password hash 
-		users = User.objects.filter(email=email_id)
+		users = CustomUser.objects.filter(email=email_id)
 		if not users:
 			return Response({'Message':"No user found in the database"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 		user = users.first()
@@ -44,10 +43,10 @@ def register(request):
 		password = json_data['password']
 		username = json_data['username']
 		# hash password
-		users = User.objects.filter(email=email_id)
+		users = CustomUser.objects.filter(email=email_id)
 		if users:
 			return Response({'Error':"Email already exists"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-		new_user = User(email = email_id,username=username, password = password)
+		new_user = CustomUser(email = email_id,username=username, password = password)
 		new_user.save()
 		return Response({'Message':"User has been created"},status=status.HTTP_200_OK)
 	else:
